28b1edc96029320fd83ec5c9534f5e9c225c294a
SameerSharma-57 <109887911+SameerSharma-57@users.noreply.github.com>
2023-02-05 19:14:57 +0530

    updated movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
     public Camera MainCamera; //be sure to assign this in the inspector to your main camera
     private Vector2 screenBounds;
     private float objectHeight;
+    private float objectWidth;
+   
     
 
     
     float vertical;
+    float horizontal;
 
     public float step = 0.1f;
 
@@ -20,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         
         screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
         objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
@@ -29,15 +33,21 @@ public class PlayerMovement : MonoBehaviour
     {
         
         vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         Vector3 temp_Pos = transform.position;
         //temp_Pos.y = Mathf.Clamp(temp_Pos.y + vertical * step, screenBounds.y - objectHeight, screenBounds.y * -1 + objectHeight);
-        temp_Pos.y=temp_Pos.y+vertical * step;
-        if(temp_Pos.y<screenBounds.y-objectHeight && temp_Pos.y > (-1)*screenBounds.y+objectHeight)
-        {
-            transform.position = temp_Pos;
-        }
         
+        
+        if(temp_Pos.y + vertical * step < screenBounds.y-objectHeight && temp_Pos.y + vertical * step > (-1)*screenBounds.y+objectHeight)
+        {
+            temp_Pos.y = temp_Pos.y + vertical * step;
+        }
+        if (temp_Pos.x + horizontal * step < screenBounds.x - objectWidth && temp_Pos.x + horizontal * step > (-1) * screenBounds.x + objectWidth)
+        {
+            temp_Pos.x = temp_Pos.x + horizontal * step;
+        }
+        transform.position = temp_Pos;
     }
 
     
